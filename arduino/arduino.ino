99973ab38fa781bbeeae5c9e9a5773dff6df8e9a
Florian Bruggisser <florian@nexpose.ch>
2017-05-20 23:24:05 +0200

    implemented basic led blinking on button press

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,14 +1,21 @@
 #define BUTTON_PIN 0
 
 boolean buttonPressed = false;
+boolean ledLightOn = false;
+
+int counter = 0;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+
+  // light up led
+  lightLED();
 
   // check if button pressed
   if (digitalRead(BUTTON_PIN) == LOW && !buttonPressed)
@@ -17,6 +24,7 @@ void loop() {
     Serial.println("button pressed!");
 
     buttonPressed = true;
+    ledLightOn = true;
   }
 
   if (digitalRead(BUTTON_PIN) == HIGH && buttonPressed)
@@ -25,7 +33,27 @@ void loop() {
     Serial.println("button released!");
 
     buttonPressed = false;
+    ledLightOn = false;
   }
 
   delay(30);
 }
+
+void lightLED()
+{
+  counter = (counter + 1) % 10;
+
+  if (ledLightOn && counter < 5)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else if (ledLightOn && counter >= 5)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
